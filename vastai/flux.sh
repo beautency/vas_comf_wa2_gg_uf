de6ac863327f7fffc3cb972aafdd0e9d4a2bd8dc
beautency <beautency@gmail.com>
2025-07-22 14:09:40 +0200

    Added workflow

--- vastai/flux.sh
+++ vastai/flux.sh
@@ -24,6 +24,7 @@ NODES=(
 )
 
 WORKFLOWS=(
+    "https://raw.githubusercontent.com/XLabs-AI/x-flux-comfyui/refs/heads/main/workflows/ip_adapter_multiple_inputs_workflow.json"
     "https://gist.githubusercontent.com/robballantyne/f8cb692bdcd89c96c0bd1ec0c969d905/raw/2d969f732d7873f0e1ee23b2625b50f201c722a5/flux_dev_example.json"
 )
 
